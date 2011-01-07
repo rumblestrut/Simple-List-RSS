e99bf37d4e667747b05a917450d885885a9878a9
Eric J. Gruber <eric@rumblestrut.com>
2011-01-07 11:18:54 -0600

    Created app with scaffolding. Item controller generated and title and url fields generated with first migration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Citynews::Application.config.secret_token = '57165a00f1f804457cabda2dcfff08449ce22a795cebb5e675d7455549bc864b7bb7cc7b10f0cbfa34242608d971a7e53e67e40e69012f424b4a0e20145ebe57'
